44b273ccd4f6aac886e5668273840619743541d7
razatechofficial <razatechofficial@gmail.com>
2026-04-30 18:14:40 +0500

    collector: streaming runner and collector stubs
    EPS-limited sinks; DefaultCollectors integration.

--- internal/collector/network_pick_other.go
+++ internal/collector/network_pick_other.go
@@ -4,8 +4,8 @@ package collector
 
 import "github.com/razatechofficial/edr/internal/config"
 
-func chooseNetworkCollector(_ config.Config, endpointID string, _ *LineageTracker) Collector {
-	if nc := NewNetworkCollector(endpointID); nc != nil {
+func chooseNetworkCollector(cfg config.Config, endpointID string, _ *LineageTracker) Collector {
+	if nc := NewNetworkCollector(endpointID, cfg); nc != nil {
 		return nc
 	}
 	return NewNetworkStubCollector(endpointID)
