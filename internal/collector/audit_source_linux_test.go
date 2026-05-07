21566a3ed18075057f9a51d39fd35eb20a82ec23
razatechofficial <razatechofficial@gmail.com>
2026-05-07 12:01:57 +0500

    Add Linux audit parser coverage

--- internal/collector/audit_source_linux_test.go
+++ internal/collector/audit_source_linux_test.go
@@ -45,6 +45,71 @@ func TestAuditSource_parseAuditBody_syscallPathCorrelates(t *testing.T) {
 	}
 }
 
+func Test_parseAuditKV_corpus(t *testing.T) {
+	cases := []struct {
+		body string
+		want map[string]string
+	}{
+		{
+			body: `type=PATH msg=audit(1.1:1): item=0 name="/tmp/my file.txt" nametype=NORMAL`,
+			want: map[string]string{
+				"type":     "PATH",
+				"item":     "0",
+				"name":     "/tmp/my file.txt",
+				"nametype": "NORMAL",
+			},
+		},
+		{
+			body: `type=SYSCALL msg=audit(1.1:2): pid=1 auid=4294967295 uid=0 comm=62617368 exe=2F62696E2F62617368`,
+			want: map[string]string{
+				"type": "SYSCALL",
+				"pid":  "1",
+				"auid": "4294967295",
+				"uid":  "0",
+				"comm": "bash",
+				"exe":  "/bin/bash",
+			},
+		},
+		{
+			body: `type=SYSCALL comm="quoted \"inner\" app" exe="/opt/foo bar/bin"`,
+			want: map[string]string{
+				"type": "SYSCALL",
+				"comm": `quoted "inner" app`,
+				"exe":  "/opt/foo bar/bin",
+			},
+		},
+	}
+	for _, tc := range cases {
+		got := parseAuditKV(tc.body)
+		for k, v := range tc.want {
+			if got[k] != v {
+				t.Fatalf("parseAuditKV(%q)[%q] = %q want %q (full=%v)", tc.body, k, got[k], v, got)
+			}
+		}
+	}
+}
+
+func TestAuditSource_multiPATHSameSerial(t *testing.T) {
+	a := NewAuditSource("ep1", "host1", nil, nil, false)
+	syscallBody := `type=SYSCALL msg=audit(1743524401.123:999): pid=1111 uid=0 comm="sh" exe="/bin/sh"`
+	path1 := `type=PATH msg=audit(1743524401.123:999): item=0 name="/a" nametype=PARENT`
+	path2 := `type=PATH msg=audit(1743524401.123:999): item=1 name="/a/b" nametype=CREATE`
+	if a.parseAuditBody(1300, syscallBody) != nil {
+		t.Fatal("syscall should not emit")
+	}
+	ev1 := a.parseAuditBody(1302, path1)
+	ev2 := a.parseAuditBody(1302, path2)
+	if ev1 == nil || ev1.File == nil || ev1.File.Path != "/a" {
+		t.Fatalf("ev1: %+v", ev1)
+	}
+	if ev2 == nil || ev2.File == nil || ev2.File.Path != "/a/b" {
+		t.Fatalf("ev2: %+v", ev2)
+	}
+	if ev2.File.ActorPID != 1111 {
+		t.Fatalf("second path lost actor pid: %d", ev2.File.ActorPID)
+	}
+}
+
 func TestAuditSource_parseAndDispatch_DedupeSkipStillAdvances(t *testing.T) {
 	a := NewAuditSource("ep1", "host1", nil, NewLinuxFileDeduper(time.Minute), false)
 	const path = "/tmp/dup"
