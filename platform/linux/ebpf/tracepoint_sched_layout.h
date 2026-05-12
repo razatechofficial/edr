165ab1b4ff2660fdd4f0a5ad5f171b518f554e56
razatechofficial <razatechofficial@gmail.com>
2026-05-12 14:13:45 +0500

    Define only sched_wakeup in platform/linux/ebpf/tracepoint_sched_layout.h. BTF vmlinux.h already includes sched_switch and sched_migrate_task layouts.

--- platform/linux/ebpf/tracepoint_sched_layout.h
+++ platform/linux/ebpf/tracepoint_sched_layout.h
@@ -1,18 +1,7 @@
 #pragma once
 
-/* Sched tracepoint raw layouts when vmlinux BTF only forward-declares them. */
+/* sched_wakeup is sometimes only forward-declared in BTF vmlinux.h. */
 #ifndef EDR_SCHED_TRACEPOINT_LAYOUTS
-
-struct trace_event_raw_sched_switch {
-	char _pad0[8];
-	char prev_comm[16];
-	pid_t prev_pid;
-	int prev_prio;
-	long long prev_state;
-	char next_comm[16];
-	pid_t next_pid;
-	int next_prio;
-};
 
 struct trace_event_raw_sched_wakeup {
 	char _pad0[8];
@@ -21,15 +10,6 @@ struct trace_event_raw_sched_wakeup {
 	int prio;
 	int success;
 	int target_cpu;
-};
-
-struct trace_event_raw_sched_migrate_task {
-	char _pad0[8];
-	char comm[16];
-	pid_t pid;
-	int prio;
-	int orig_cpu;
-	int dest_cpu;
 };
 
 #endif /* EDR_SCHED_TRACEPOINT_LAYOUTS */
